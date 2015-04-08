4dda9291d6bdc72f98de198a54e7d7fb752b8399
Grace Ng <grace@leanstartupmachine.com>
2015-04-08 13:36:38 -0400

    star rating works

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,3 +8,37 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function(){
+
+var w = $(window).width();
+var h = $(window).height();
+var exp = $('.widget');
+
+exp.css('top', h/2);
+
+
+//FULL SCREEN 
+var c = 1;
+$('.fa-expand').click(function() {
+c++;
+
+$(this).toggleClass('fa-compress');
+
+//odd -> Full Screen 
+if(c%2==0){
+
+exp.animate({width:w}, 300, function(){
+$(this).animate({height:h}, 300);
+exp.addClass('active');
+});
+}
+
+//even -> Exit Full Screen 
+if (c%2!=0){
+
+exp.animate({height:'480'}, 300, function(){
+$(this).animate({width:'850'}, 300);
+exp.removeClass('active');
+});
+}
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def markdown(text)
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true, filter_html:true), underline: true, quote: true, highlight: true, strikethrough: true, autolink: true)
+    renderer.render(text).html_safe
+  end 
 end
+
+
